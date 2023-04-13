d8194d2e9ef22ec3516840b73d39eaba26b60dba
tuba.koseoglu <tbakoseoglu@gmail.com>
2023-04-13 15:19:34 +0300

    General design & chart

--- CalculateDistance-Arduino/MainWindow.xaml.cs
+++ CalculateDistance-Arduino/MainWindow.xaml.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,6 +27,69 @@ namespace CalculateDistance_Arduino
         public MainWindow()
         {
             InitializeComponent();
+
+            //get active ports in pc for connect to Arduino
+            GetActivePorts();
+
+            //create db for chart
+            var viewModel = new MainViewModel();
+            DataContext = viewModel;
+        }
+
+        private void ConnectToArduino_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TestConnection_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DisconnectFromArduino_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetActivePorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (var item in ports)
+            {
+                cmbPort.Items.Add(item);
+            }
+        }
+
+        private void ConnectSerialPort()
+        {
+            //baudrate : 57600 -> ideal for serial connection
+            //parity : none -> one way communication
+            //databit : 8 -> ideal for serial connection
+            //stopbits : one -> ideal for serial connection
+            SerialPort serialPort = new SerialPort(
+                    cmbPort.SelectedItem.ToString() ?? throw new ArgumentNullException(nameof(cmbPort.SelectedItem)),
+                    57600,
+                    Parity.None,
+                    8,
+                    StopBits.One);
+        }
+
+        private void txtConnectionStatus_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtConnectionStatus.Text == "Not Connected")
+            {
+                txtConnectionStatus.Foreground = Brushes.Red;
+            }
+            else
+            {
+                txtConnectionStatus.Foreground = Brushes.ForestGreen;
+            }
         }
     }
 }
+
