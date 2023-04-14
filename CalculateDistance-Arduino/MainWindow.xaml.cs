617c10bc9c7283f457877a5222a64e0d7c235231
tuba.koseoglu <tbakoseoglu@gmail.com>
2023-04-14 23:17:23 +0300

    Bluetooth connection

--- CalculateDistance-Arduino/MainWindow.xaml.cs
+++ CalculateDistance-Arduino/MainWindow.xaml.cs
@@ -24,46 +24,19 @@ namespace CalculateDistance_Arduino
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SerialPort? serialPort;
+        private ViewModelChart viewModelChart;
+        private int counter = 1;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            //get active ports in pc for connect to Arduino
-            GetActivePorts();
-
             //create db for chart
-            var viewModel = new MainViewModel();
-            DataContext = viewModel;
+            viewModelChart = new ViewModelChart();
+            DataContext = viewModelChart;
         }
 
-        private void ConnectToArduino_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Exit_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TestConnection_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void DisconnectFromArduino_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void GetActivePorts()
-        {
-            string[] ports = SerialPort.GetPortNames();
-            foreach (var item in ports)
-            {
-                cmbPort.Items.Add(item);
-            }
-        }
 
         private void ConnectSerialPort()
         {
@@ -71,24 +44,106 @@ namespace CalculateDistance_Arduino
             //parity : none -> one way communication
             //databit : 8 -> ideal for serial connection
             //stopbits : one -> ideal for serial connection
-            SerialPort serialPort = new SerialPort(
-                    cmbPort.SelectedItem.ToString() ?? throw new ArgumentNullException(nameof(cmbPort.SelectedItem)),
-                    57600,
-                    Parity.None,
-                    8,
-                    StopBits.One);
+            //serialPort = new SerialPort(
+            //        cmbPort.SelectedItem.ToString() ?? throw new ArgumentNullException(nameof(cmbPort.SelectedItem)),
+            //        57600,
+            //        Parity.None,
+            //        8,
+            //        StopBits.One);
+
+            if (txtBaudrate.Text == "")
+            {
+                MessageBox.Show("Please enter baudrate for connection.","ERROR");
+                return;
+            }
+
+            serialPort = new SerialPort(txtPort.Text, Convert.ToInt32(txtBaudrate.Text));
+            serialPort.Open();
+            serialPort.DataReceived += SerialPort_DataReceived;
+
+            if (serialPort.IsOpen)
+            {
+                txtConnectionStatus.Text = "Connected";
+               
+            }
+            else
+            {
+                txtConnectionStatus.Text = "Not Connected";
+                MessageBox.Show("Serial port cannot connect.","ERROR");
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //if (serialPort == null || !(serialPort.IsOpen))
+            //{
+            //    return;
+            //}
+
+            ////read data from HM-10
+            //string data = serialPort.ReadLine();
+            //int distance;
+            //if (int.TryParse(data, out distance))
+            //{
+            //  //  viewModelChart.AddDataToViewModel(i++, i);
+            //    // string veri başarıyla tamsayıya dönüştürüldü, işlemlere devam edebilirsiniz
+            //}
+            //else
+            //{
+            //    // string veri tamsayıya dönüştürülemedi, hata mesajı gösterilebilir
+            //}
+
+            //// add data to chart
+            //Dispatcher.Invoke(() =>
+            //{
+            //    viewModelChart.AddDataToViewModel(distance, counter);
+            //    counter++;
+            //});
         }
 
         private void txtConnectionStatus_TextChanged(object sender, TextChangedEventArgs e)
         {
+
             if (txtConnectionStatus.Text == "Not Connected")
             {
                 txtConnectionStatus.Foreground = Brushes.Red;
+                menuConnectBluetooth.IsEnabled = true;
+                menuDisconnect.IsEnabled = false;
             }
             else
             {
                 txtConnectionStatus.Foreground = Brushes.ForestGreen;
+                menuConnectBluetooth.IsEnabled = false;
+                menuDisconnect.IsEnabled = true;
             }
+        }
+
+        private void menuConnectBluetooth_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectSerialPort();
+        }
+
+        private void menuDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (serialPort != null)
+            {
+                serialPort.Close();
+                serialPort.DataReceived -= SerialPort_DataReceived;
+            }
+
+            txtConnectionStatus.Text = "Not Connected";
+        }
+
+        private void menuTest_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                viewModelChart.AddDataToViewModel(i++, i);
+
+            }
+
+            //chartDistances.Series[0].Points.AddXY(xValue, yValue);
+
         }
     }
 }
---
+++ CalculateDistance-Arduino/ViewModelChart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateDistance_Arduino
+{
+    public class ViewModelChart
+    {
+        public ViewModelChart()
+        {
+            //Data = new ObservableCollection<DataPoint>
+            //{
+            //    new DataPoint(1, 10),
+            //    new DataPoint(2, 20),
+            //    new DataPoint(3, 30),
+            //    new DataPoint(4, 40),
+            //    new DataPoint(5, 50)
+            //};
+            Data = new ObservableCollection<DataPoint>();
+        }
+
+        public void AddDataToViewModel(int distance, int time)
+        {
+            Data.Add(new DataPoint(distance, time));
+        }
+
+        public ObservableCollection<DataPoint> Data { get; }
+    }
+}
